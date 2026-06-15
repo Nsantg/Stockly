5717e49b2902d0d4151b54b2be81f321e077d1fe
jjquintero_9 <jjquintero_9@cue.edu.co>
2026-06-15 14:11:44 -0500

    fix: toast ocupa ancho completo en movil

--- src/components/ui/Toast.tsx
+++ src/components/ui/Toast.tsx
@@ -32,7 +32,7 @@ export function ToastProvider({ children }: { children: React.ReactNode }) {
   return (
     <ToastContext.Provider value={{ toast }}>
       {children}
-      <div className="fixed bottom-5 right-5 z-50 flex flex-col gap-2 pointer-events-none">
+      <div className="fixed bottom-4 left-4 right-4 sm:bottom-5 sm:left-auto sm:right-5 sm:w-auto z-50 flex flex-col gap-2 pointer-events-none">
         {toasts.map((t) => (
           <div
             key={t.id}
